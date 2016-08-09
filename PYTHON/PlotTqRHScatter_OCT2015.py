296b8805e4180fc643443bc6a29a8704e1245a05
Kate Willett <kate.willett@metoffice.gov.uk>
2016-08-09 17:12:46 +0100

    updated to also plot TRh by q and function for vmin/vmax/bounds

--- PYTHON/PlotTqRHScatter_OCT2015.py
+++ PYTHON/PlotTqRHScatter_OCT2015.py
@@ -30,7 +30,7 @@
 # import numpy as np
 # import numpy.ma as ma
 # import sys, os
-# import scipy.stats
+# import scipy.stats as ss # for pearsonr
 # import struct
 # from mpl_toolkits.basemap import Basemap
 # import datetime as dt
@@ -80,6 +80,16 @@
 # VERSION/RELEASE NOTES
 # -----------------------
 # 
+# Version 2 9 August 2016
+# ---------
+#  
+# Enhancements
+# Can also plot T vs RH coloured by q anomaly
+#  
+# Changes
+#  
+# Bug fixes
+#
 # Version 1 8 October 2015
 # ---------
 #  
@@ -99,7 +109,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import numpy.ma as ma
 import sys, os
-import scipy.stats
+import scipy.stats as ss # for pearsonr
 import struct
 from mpl_toolkits.basemap import Basemap
 import datetime as dt
@@ -113,11 +123,11 @@ import pdb #stop: pdb.set_trace(), start: c
 TimeRes='Y'	# M=month, Y=year	
 Region='A'	# A=All, G=Globe, N=NHemi, T=Tropics, S=SHemi
 homogtype='IDPHA'	# 'IDPHA','PHA','PHADPD'
-nowmon='OCT'
-nowyear='2015'
+nowmon='AUG'
+nowyear='2016'
 
 styr=1973
-edyr=2014
+edyr=2015
 nyrs=(edyr-styr)+1
 nmons=(nyrs)*12
 if (TimeRes == 'Y'):
@@ -128,14 +138,15 @@ YrStr=np.array(range(styr,edyr+1),dtype=str)
 YrStr=np.array(([i[2:5] for i in YrStr])) # now a string array of the last two digits
 
 # Set up directories and files
-INDIR='/data/local/hadkw/HADCRUH2/UPDATE2014/STATISTICS/TIMESERIES/'
-OUTDIR='/data/local/hadkw/HADCRUH2/UPDATE2014/IMAGES/ANALYSIS/'
+INDIR='/data/local/hadkw/HADCRUH2/UPDATE2015/STATISTICS/TIMESERIES/'
+OUTDIR='/data/local/hadkw/HADCRUH2/UPDATE2015/IMAGES/ANALYSIS/'
 
-In_q='HadISDH.landq.2.0.1.2014p_FLATgridIDPHA5by5_JAN2015_areaTS_19732014.nc'
-In_RH='HadISDH.landRH.2.0.1.2014p_FLATgridIDPHA5by5_JAN2015_areaTS_19732014.nc'
-In_T='HadISDH.landT.2.0.1.2014p_FLATgridIDPHA5by5_JAN2015_areaTS_19732014.nc'
+In_q='HadISDH.landq.2.1.0.2015p_FLATgridIDPHA5by5_JAN2016_anoms7605_areaTS_19732015.nc'
+In_RH='HadISDH.landRH.2.1.0.2015p_FLATgridIDPHA5by5_JAN2016_anoms7605_areaTS_19732015.nc'
+In_T='HadISDH.landT.2.1.0.2015p_FLATgridIDPHA5by5_JAN2016_anoms7605_areaTS_19732015.nc'
 
-OutPlot='ScatterTqRH_HadISDH.2.0.1.2014p_'+Region+'_'+nowmon+nowyear
+OutPlotTq='ScatterTqbyRH_HadISDH.2.1.0.2015p_'+Region+'_'+nowmon+nowyear
+OutPlotTRH='ScatterTRHbyq_HadISDH.2.1.0.2015p_'+Region+'_'+nowmon+nowyear
  
 # Set up variables
 q_arr=0	#set once file read in
@@ -168,9 +179,63 @@ def ReadNetCDFTS(FileName,ReadInfo,TheData):
     return TheData # ReadNetCDFTS
 
 #************************************************************************
+# MakeUpSteps
+def MakeUpSteps(TheArray,stepsies=9):
+    ''' Given a max and min, make up NICE step sizes for a 9 element colourbar '''
+    ''' Currently works with a minimum range of 0.2 and a maximum or 3.0 '''
+    ''' Can only deal with symmetric ranges '''
+    ''' READS: 	TheArray - an array of data '''
+    '''		stepsies (OPTIONAL) - number of colours in colourbar - default 9 is NICE '''
+    ''' RETURNS: vmin - minimum threshold of range '''
+    '''		 vmax - maximum threshold of range '''
+    '''		 bounds - stepsies linear increments through the range from vmin to vmax '''
+    ''' 	 strcounds - strings of the bounds for labelling the colourbar '''
+
+    vmax=np.int(np.ceil(np.max(abs(TheArray))*10))/10.    
+    vmin=-vmax
+    nsteps = stepsies
+    if (vmax <= 0.2):
+        vmax = 0.2
+	vmin = -0.2
+    if (vmax <= 0.3):
+        vmax = 0.32
+	vmin = -0.32
+    elif (vmax <= 0.4):
+        vmax = 0.4
+	vmin = -0.4
+    elif (vmax <= 0.6):
+        vmax = 0.6
+	vmin = -0.6
+    elif (vmax <= 0.8):
+        vmax = 0.8
+	vmin = -0.8
+    elif (vmax <= 1.0):
+        vmax = 1.0
+	vmin = -1.0
+    elif (vmax <= 1.2):
+        vmax = 1.2
+	vmin = -1.2
+    elif (vmax <= 1.6):
+        vmax = 1.6
+	vmin = -1.6
+    elif (vmax <= 2.0):
+        vmax = 2.0
+	vmin = -2.0
+    elif (vmax <= 3.0):
+        vmax = 3.0
+	vmin = -3.0
+	#    pdb.set_trace() # stop here and play
+
+    bounds=np.linspace(vmin,vmax,nsteps)
+    strbounds=["%4.1f" % i for i in bounds]
+        
+    return vmax,vmin,strbounds,bounds
+
+#************************************************************************
 # PlotScatter
-def PlotScatter(TheFile,TheYrStr,Thentims,Theq_arr,TheRH_arr,TheT_arr,TheReg):
+def PlotScatter(TheFileTq,TheFileTRH,TheYrStr,Thentims,Theq_arr,TheRH_arr,TheT_arr,TheReg):
     ''' Plot Tq scatter with colours related to RH'''
+    ''' Plot TRH scatter with colours related to q'''
     ''' Points are either the last two years YY or MONYY '''
     ''' Save as png and eps '''
     ''' TheFile - the filepath and filename for the image '''
@@ -205,33 +270,11 @@ def PlotScatter(TheFile,TheYrStr,Thentims,Theq_arr,TheRH_arr,TheT_arr,TheReg):
 
     cmap=cmap.from_list('this_cmap',cmaplist,cmap.N)
     
-    # work out best max and min values for colourbar and try to make them 'nice
-    # must be an odd number of steps
-    # for less than 0.1 must be 14 or fewer steps
-    vmax=np.int(np.ceil(np.max(abs(TheRH_arr))*10))/10.    
-    vmin=-vmax
-    if (vmax <= 0.3):
-        nsteps=np.int((vmax-vmin)/0.05)+1
-    elif (vmax <= 0.5):
-        vmax=np.ceil(np.max(abs(TheRH_arr))/0.06)*0.06  
-        vmin=-vmax
-        nsteps=np.int((vmax-vmin)/0.06)+1
-    elif (vmax <= 1.0):
-        vmax=np.ceil(np.max(abs(TheRH_arr))/0.1)*0.1  
-        vmin=-vmax
-        nsteps=np.int((vmax-vmin)/0.1)+1
-    elif (vmax <= 1.4):
-        vmax=np.ceil(np.max(abs(TheRH_arr))/0.2)*0.2  
-        vmin=-vmax
-        nsteps=np.int((vmax-vmin)/0.2)+1
-    elif (vmax > 1.4):
-        vmax=np.ceil(np.max(abs(TheRH_arr))/0.3)*0.3  
-        vmin=-vmax
-        nsteps=np.int((vmax-vmin)/0.3)+1
-	#    pdb.set_trace() # stop here and play
+    # FIRST MAKE UP THE TqbyRH plot
+
+    # Call MakeUpSteps routine to get a NICE set of colourbar indices
+    vmin,vmax,strbounds,bounds=MakeUpSteps(TheRH_arr)
     
-    bounds=np.linspace(vmin,vmax,nsteps)
-    strbounds=["%4.1f" % i for i in bounds]
     norm=mpl_cm.colors.BoundaryNorm(bounds,cmap.N)
         
     ytitlee='Specific Humidity Anomalies (g kg$^{-1}$)'
@@ -259,6 +302,9 @@ def PlotScatter(TheFile,TheYrStr,Thentims,Theq_arr,TheRH_arr,TheT_arr,TheReg):
         # make blank plot with zero lines on
 	ax1.plot(np.zeros(100),np.linspace(qmin,qmax,100),color='black',linewidth=2)
 	ax1.plot(np.linspace(tmin,tmax,100),np.zeros(100),color='black',linewidth=2)
+	
+	# plot 1:1 line dashed
+	ax1.plot(np.linspace(-5,5,100),np.linspace(-5,5,100),color='black',linewidth=2,linestyle='dashed')
 
         # plot black dots for the goods
         for vv in range(Thentims):
@@ -289,6 +335,14 @@ def PlotScatter(TheFile,TheYrStr,Thentims,Theq_arr,TheRH_arr,TheT_arr,TheReg):
 	    PointTitle=3
         ax1.set_title(titlees[PointTitle],size=18)
 
+        # Get correlation and slope of scatter and add to plot
+	#pcorr = ss.pearsonr(TheT_arr[0,:],Theq_arr[0,:]) # element 0 = pearson correlation coefficient, element 1 = two-tailed p-value 
+	linvals = ss.linregress(TheT_arr[0,:],Theq_arr[0,:]) # 0 = slope, 1 = intercept, 2 = r-value, 3 = two-tailed p-value, 4 = sterr
+        plt.figtext(0.05,0.96,'r = '+"{:3.2f}".format(linvals[2]),size=12)
+        plt.figtext(0.05,0.9,'m = '+"{:3.2f}".format(linvals[0]),size=12)
+        plt.figtext(0.05,0.84,'p = '+"{:1.2f}".format(linvals[3]),size=12)
+
+
     else:
 
         # Four plot scenario
@@ -311,6 +365,9 @@ def PlotScatter(TheFile,TheYrStr,Thentims,Theq_arr,TheRH_arr,TheT_arr,TheReg):
 	    ax[pp].plot(np.zeros(100),np.linspace(qmin,qmax,100),color='black',linewidth=2)
 	    ax[pp].plot(np.linspace(tmin,tmax,100),np.zeros(100),color='black',linewidth=2)
 
+	    # plot 1:1 line dashed
+	    ax[pp].plot(np.linspace(-5,5,100),np.linspace(-5,5,100),color='black',linewidth=2,linestyle='dashed')
+
             # plot black dots for the goods
             for vv in range(Thentims):
 	        scats=ax[pp].scatter(TheT_arr[pp,vv],Theq_arr[pp,vv],c=TheRH_arr[pp,vv],marker=r"$ {} $".format(Pointees[vv]),s=200,cmap=cmap,norm=norm, edgecolors='none' ) # s=1
@@ -328,6 +385,13 @@ def PlotScatter(TheFile,TheYrStr,Thentims,Theq_arr,TheRH_arr,TheT_arr,TheReg):
             plt.figtext((xstart[pp]+0.02),ystart[pp]+ytall-0.05,TheLetter[pp],size=14)
         
             ax[pp].set_title(titlees[pp],size=14)
+
+            # Get correlation and slope of scatter and add to plot
+	    #pcorr = ss.pearsonr(TheT_arr[pp,:],Theq_arr[pp,:]) # element 0 = pearson correlation coefficient, element 1 = two-tailed p-value 
+	    linvals = ss.linregress(TheT_arr[pp,:],Theq_arr[pp,:]) # 0 = slope, 1 = intercept, 2 = r-value, 3 = two-tailed p-value, 4 = sterr
+            plt.figtext((xstart[pp]+0.05),ystart[pp]+ytall-0.05,'r = '+"{:3.2f}".format(linvals[2]),size=12)
+            plt.figtext((xstart[pp]+0.05),ystart[pp]+ytall-0.07,'m = '+"{:3.2f}".format(linvals[0]),size=12)
+            plt.figtext((xstart[pp]+0.05),ystart[pp]+ytall-0.09,'p = '+"{:1.2f}".format(linvals[3]),size=12)
         
 	cbax=fig.add_axes([0.86,0.1,0.03,0.8])
         cb=plt.colorbar(scats,cax=cbax,orientation='vertical',ticks=bounds) #, extend=extend
@@ -340,12 +404,154 @@ def PlotScatter(TheFile,TheYrStr,Thentims,Theq_arr,TheRH_arr,TheT_arr,TheReg):
 
 
     #plt.show()
-    plt.savefig(TheFile+".eps")
-    plt.savefig(TheFile+".png")
+    plt.savefig(TheFileTq+".eps")
+    plt.savefig(TheFileTq+".png")
 
 #    raw_input("stop")	# REALLY USEFUL TO INTERACT WITHIN SUBROUTINE ctrl C
     # plt.ion()
     # plt.show() can then zoom and save
+
+    #***********************************
+    # SECOND MAKE UP THE TRHbyq plot
+
+    # Call MakeUpSteps routine to get a NICE set of colourbar indices
+    vmin,vmax,strbounds,bounds=MakeUpSteps(Theq_arr)
+    
+    norm=mpl_cm.colors.BoundaryNorm(bounds,cmap.N)
+        
+    ytitlee='Relative Humidity Anomalies (%rh)'
+    xtitlee='Temperature Anomalies ($^{o}$C)'
+    titlees=['Globe 70$^{o}$S to 70$^{o}$N','N. Hemisphere 20$^{o}$N to 70$^{o}$N','Tropics 20$^{o}$S to 20$^{o}$N','S. Hemisphere 70$^{o}$S to 20$^{o}$S']
+
+    # set up max and min of RH and T for axes - keep same for all regions
+    rhmax=np.ceil(np.max(abs(TheRH_arr))/0.1)*0.1  
+    rhmin=-rhmax
+    tmax=np.ceil(np.max(abs(TheT_arr))/0.1)*0.1  
+    tmin=-tmax
+                
+    # set up plot - are we working with one region or four?
+    if (TheReg != 'A'):
+
+        # Single plot scenario
+        fig = plt.figure(1,figsize=(8,8))
+        plt.clf()	# needs to be called after figure!!! (create the figure, then clear the plot space)
+
+        ax1=plt.axes([0.1,0.1,0.75,0.8]) # left, bottom, width, height
+
+        ax1.set_xlim([tmin,tmax])   
+        ax1.set_ylim([rhmin,rhmax])   
+
+        # make blank plot with zero lines on
+	ax1.plot(np.zeros(100),np.linspace(rhmin,rhmax,100),color='black',linewidth=2)
+	ax1.plot(np.linspace(tmin,tmax,100),np.zeros(100),color='black',linewidth=2)
+
+	# plot 1:1 line dashed
+	ax1.plot(np.linspace(-5,5,100),np.linspace(-5,5,100),color='black',linewidth=2,linestyle='dashed')
+
+        # plot YEAR LABELS for the goods
+        for vv in range(Thentims):
+	    #print(vv,TheT_arr[0,vv],Theq_arr[0,vv],TheRH_arr[0,vv],r"$ {} $".format(Pointees[vv]))
+	    scats=ax1.scatter(TheT_arr[0,vv],TheRH_arr[0,vv],c=Theq_arr[0,vv],marker=r"$ {} $".format(Pointees[vv]),s=250,cmap=cmap,norm=norm, edgecolors='none' ) # s=1
+
+        ax1.set_xlabel(xtitlee,size=14)
+        ax1.set_ylabel(ytitlee,size=14)
+        ax1.tick_params(axis='both', which='major', labelsize=14)
+        
+	cbax=fig.add_axes([0.86,0.1,0.03,0.8])
+        cb=plt.colorbar(scats,cax=cbax,orientation='vertical',ticks=bounds) #, extend=extend
+        cb.ax.tick_params(labelsize=14) 
+        plt.figtext(0.97,0.5,'q Anomalies (g kg$^{-1}$)',size=14,ha='center',rotation='vertical',va='center')
+
+    # add watermark and plot labels
+#    watermarkstring="/".join(os.getcwd().split('/')[4:])+'/'+os.path.basename( __file__ )+"   "+dt.datetime.strftime(dt.datetime.now(), "%d-%b-%Y %H:%M")
+#    plt.figtext(0.01,0.01,watermarkstring,size=6)
+
+#    plt.figtext(0.02,0.96,TheLetter,size=18)
+        if (TheReg == 'G'):
+	    PointTitle=0
+        if (TheReg == 'N'):
+	    PointTitle=1
+        if (TheReg == 'T'):
+	    PointTitle=2
+        if (TheReg == 'S'):
+	    PointTitle=3
+        ax1.set_title(titlees[PointTitle],size=18)
+
+        # Get correlation and slope of scatter and add to plot
+	#pcorr = ss.pearsonr(TheT_arr[0,:],TheRH_arr[0,:]) # element 0 = pearson correlation coefficient, element 1 = two-tailed p-value 
+	linvals = ss.linregress(TheT_arr[0,:],TheRH_arr[0,:]) # 0 = slope, 1 = intercept, 2 = r-value, 3 = two-tailed p-value, 4 = sterr
+        plt.figtext(0.05,0.96,'r = '+"{:3.2f}".format(linvals[2]),size=12)
+        plt.figtext(0.05,0.9,'m = '+"{:3.2f}".format(linvals[0]),size=12)
+        plt.figtext(0.05,0.84,'p = '+"{:1.2f}".format(linvals[3]),size=12)
+
+    else:
+
+        # Four plot scenario
+        fig,ax=plt.subplots(4,figsize=(8,8))	#6,18
+        plt.clf()	# needs to be called after figure!!! (create the figure, then clear the plot space)
+        TheLetter=['a)','b)','c)','d)']
+	
+	xstart=[0.1,0.48,0.1,0.48]
+	xwide=0.36
+	ystart=[0.54,0.54,0.08,0.08]
+	ytall=0.36
+
+	for pp in range(4):
+            ax[pp]=plt.axes([xstart[pp],ystart[pp],xwide,ytall]) # left, bottom, width, height
+
+            ax[pp].set_xlim([tmin,tmax])   
+            ax[pp].set_ylim([rhmin,rhmax])   
+
+            # make blank plot with zero lines on
+	    ax[pp].plot(np.zeros(100),np.linspace(rhmin,rhmax,100),color='black',linewidth=2)
+	    ax[pp].plot(np.linspace(tmin,tmax,100),np.zeros(100),color='black',linewidth=2)
+
+	    # plot 1:1 line dashed
+	    ax[pp].plot(np.linspace(-5,5,100),np.linspace(-5,5,100),color='black',linewidth=2,linestyle='dashed')
+
+            # plot black dots for the goods
+            for vv in range(Thentims):
+	        scats=ax[pp].scatter(TheT_arr[pp,vv],TheRH_arr[pp,vv],c=Theq_arr[pp,vv],marker=r"$ {} $".format(Pointees[vv]),s=200,cmap=cmap,norm=norm, edgecolors='none' ) # s=1
+
+            if (pp == 2) | (pp == 3):
+	        ax[pp].set_xlabel(xtitlee,size=12)
+            if (pp == 0) | (pp == 2):
+                ax[pp].set_ylabel(ytitlee,size=12)
+	    if (pp == 0) | (pp == 1):	
+		ax[pp].xaxis.set_ticklabels([])
+	    if (pp == 1) | (pp == 3):	
+		ax[pp].yaxis.set_ticklabels([])
+            ax[pp].tick_params(axis='both', which='major', labelsize=12)
+
+            plt.figtext((xstart[pp]+0.02),ystart[pp]+ytall-0.05,TheLetter[pp],size=14)
+        
+            ax[pp].set_title(titlees[pp],size=14)
+
+            # Get correlation and slope of scatter and add to plot
+	    #pcorr = ss.pearsonr(TheT_arr[pp,:],TheRH_arr[pp,:]) # element 0 = pearson correlation coefficient, element 1 = two-tailed p-value 
+	    linvals = ss.linregress(TheT_arr[pp,:],TheRH_arr[pp,:]) # 0 = slope, 1 = intercept, 2 = r-value, 3 = two-tailed p-value, 4 = sterr
+            plt.figtext((xstart[pp]+0.05),ystart[pp]+ytall-0.05,'r = '+"{:3.2f}".format(linvals[2]),size=12)
+            plt.figtext((xstart[pp]+0.05),ystart[pp]+ytall-0.07,'m = '+"{:3.2f}".format(linvals[0]),size=12)
+            plt.figtext((xstart[pp]+0.05),ystart[pp]+ytall-0.09,'p = '+"{:1.2f}".format(linvals[3]),size=12)
+        
+	cbax=fig.add_axes([0.86,0.1,0.03,0.8])
+        cb=plt.colorbar(scats,cax=cbax,orientation='vertical',ticks=bounds) #, extend=extend
+        cb.ax.tick_params(labelsize=12) 
+        plt.figtext(0.97,0.5,'q Anomalies (g kg$^{-1}$)',size=12,ha='center',rotation='vertical',va='center')
+
+    # add watermark and plot labels
+#    watermarkstring="/".join(os.getcwd().split('/')[4:])+'/'+os.path.basename( __file__ )+"   "+dt.datetime.strftime(dt.datetime.now(), "%d-%b-%Y %H:%M")
+#    plt.figtext(0.01,0.01,watermarkstring,size=6)
+
+
+    #plt.show()
+    plt.savefig(TheFileTRH+".eps")
+    plt.savefig(TheFileTRH+".png")
+
+#    raw_input("stop")	# REALLY USEFUL TO INTERACT WITHIN SUBROUTINE ctrl C
+    # plt.ion()
+    # plt.show() can then zoom and save
+
      
     return #PlotNiceDotsMap
     
@@ -417,8 +623,9 @@ for rr in range(nReg):
         T_arr[rr,:]=tmpT_arr[rr,:]
     
 # Plot the scatter
-MyFile=OUTDIR+OutPlot
-PlotScatter(MyFile,YrStr,ntims,q_arr,RH_arr,T_arr,Region)
+MyFileTq=OUTDIR+OutPlotTq
+MyFileTRH=OUTDIR+OutPlotTRH
+PlotScatter(MyFileTq,MyFileTRH,YrStr,ntims,q_arr,RH_arr,T_arr,Region)
 		
 #    stop()
 
