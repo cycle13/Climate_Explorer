Corrected RH bug in es_ice calculation and also corrected the np.where()[0] so that len() works correctly AND made sure it works with both numpy arrays and scalars which it now does

PYTHON/CalcHums.py
@@ -19,7 +19,7 @@ dew point temperature from vapour pressure and temperature and pressure
 relative humidity from vapour pressure and temperature and pressure
 wet bulb temperature from vapour pressure and dew point temperature and temperature (and pressure?)
 
-Where vapour pressure is used as part of the equation a pseudo wet bulb 
+Where vapour pressure or saturated vapour pressure is used as part of the equation a pseudo wet bulb 
 temperature is calculated. If this is at or below 0 deg C then the ice bulb
 equation is used.
 
@@ -29,6 +29,10 @@ THIS ROUTINE CANNOT COPE WITH MISSING DATA
 
 THIS ROUTINE HAS a roundit=True/False. The default is True - round to one decimal place.
 Otherwise - set roundit=False
+
+THIS ROUTINE CAN COPE WITH SCALARS OR ARRAYS AND WILL RETURN WHATEVER IS PUT IN
+
+T and TD must be identical length. P can be a scalar even if T and Td are arrays
 
 '''
 
@@ -70,6 +74,21 @@ def vap(td,t,p,roundit=True):
     e = 12.3
 
     '''
+    
+    # Make sure everything is a numpy array
+    # At the end return a scalar if it was a scalar to start with
+    # T and Td have to be identical length so if one is a scalar then the other should be
+    ScalarFlag = 0
+    if (np.isscalar(td) == True):
+    
+        # We have scalars so temporarily make them numpy arrays
+	td = np.array([td])
+        t = np.array([t])
+	p = np.array([p])
+	
+	# Set the scalar flag to 1 so that we convert back again
+	ScalarFlag = 1    
+
     e = None
 
     # Calculate pseudo-e assuming wet bulb to calculate a pseudo-wet bulb (see wb below)
@@ -86,7 +105,8 @@ def vap(td,t,p,roundit=True):
     # to establish whether to calculate e with respect to ice or water
     # recalc if ice
     # Find instances of ice
-    icy = np.where(w <= 0.0) # no [0] at the end of np.where() because we need a pointer array rather than a np.array we can work witth
+    icy = np.where(w <= 0.0)[0] # no [0] at the end of np.where() because we need a pointer array rather than a np.array we can work witth
+    # Actually we do need [0] because when testing for length without [0] it always has length 1. Still works as a pointer
     if (len(icy) > 0):    
     
 #        print('found an icy')
@@ -105,6 +125,11 @@ def vap(td,t,p,roundit=True):
 	
     if (roundit == True):
         e = np.round(e*10)/10.	
+	
+    if (ScalarFlag == 1):
+    
+        # Return a scalar
+	e = e[0]
 	     
     return e
 
@@ -171,6 +196,21 @@ def sh(td,t,p,roundit=True):
     sh = 7.6
 
     '''
+
+    # Make sure everything is a numpy array
+    # At the end return a scalar if it was a scalar to start with
+    # T and Td have to be identical length so if one is a scalar then the other should be
+    ScalarFlag = 0
+    if (np.isscalar(td) == True):
+    
+        # We have scalars so temporarily make them numpy arrays
+	td = np.array([td])
+        t = np.array([t])
+	p = np.array([p])
+	
+	# Set the scalar flag to 1 so that we convert back again
+	ScalarFlag = 1    
+
     q = None
 
     e = None
@@ -187,7 +227,7 @@ def sh(td,t,p,roundit=True):
     # to establish whether to calculate e with respect to ice or water
     # recalc if ice
     # Find instances of ice
-    icy = np.where(w <= 0.0)
+    icy = np.where(w <= 0.0)[0]
     if (len(icy) > 0):    
     
         # Need to check whether pressure is an array or scalar
@@ -206,6 +246,11 @@ def sh(td,t,p,roundit=True):
 
     if (roundit == True):
         q = np.round(q*10.)/10.
+	
+    if (ScalarFlag == 1):
+    
+        # Return a scalar
+	q = q[0]
 	
     return q
 
@@ -276,6 +321,20 @@ def rh(td,t,p,roundit=True):
     rh = 72.0
     
     '''
+    # Make sure everything is a numpy array
+    # At the end return a scalar if it was a scalar to start with
+    # T and Td have to be identical length so if one is a scalar then the other should be
+    ScalarFlag = 0
+    if (np.isscalar(td) == True):
+    
+        # We have scalars so temporarily make them numpy arrays
+	td = np.array([td])
+        t = np.array([t])
+	p = np.array([p])
+	
+	# Set the scalar flag to 1 so that we convert back again
+	ScalarFlag = 1    
+    
     r = None
 
     e = None
@@ -288,11 +347,14 @@ def rh(td,t,p,roundit=True):
     b = ((409.8*e) / ((td + 237.3)**2))
     w = (((a*t) + (b*td)) / (a + b))
 
+    #print(e,w)
+
     # Now test for whether pseudo-wetbulb is above or below/equal to zero
     # to establish whether to calculate e with respect to ice or water
     # recalc if ice
+    # This will be different if not a numpy array so test length!
     # Find instances of ice
-    icy = np.where(w <= 0.0)
+    icy = np.where(w <= 0.0)[0]
     if (len(icy) > 0):    
     
         # Need to check whether pressure is an array or scalar
@@ -302,6 +364,8 @@ def rh(td,t,p,roundit=True):
             f = 1 + (3.*(10**(-4.))) + ((4.18*(10**(-6.)))*p[icy])
 	
         e[icy] = 6.1115*f*np.exp(((23.036 - (td[icy]/333.7))*td[icy]) / (279.82+td[icy]))
+	
+	#print('ICY ',e)
 
 #    if (w <= 0.0):
 #        f = 1 + (3.*(10**(-4.))) + ((4.18*(10**(-6.)))*p)
@@ -311,18 +375,22 @@ def rh(td,t,p,roundit=True):
 
     # Calculate pseudo-es assuming wet bulb to calculate a pseudo-wet bulb (see wb below)
     # USING t INSTEAD OF td FOR SATURATED VAPOUR PRESSURE (WET BULB T = T AT SATURATION)
+    # BIG Q here - how to calculate wb based on es or do we use wb based on e?
+    # If we calc based on es we swap td for t?
     f = 1 + (7.*(10**(-4.))) + ((3.46*(10**(-6.)))*p)
     es = 6.1121*f*np.exp(((18.729 - (t/227.3))*t) / (257.87 + t))
 
     a = 0.000066*p
     b = ((409.8*es) / ((t + 237.3)**2)) # t here rather than td because for es, t==td
     w = (((a*t) + (b*t)) / (a + b)) # second t is t here rather than td because for ex, t==td
+    
+    #print(es,w)
 
     # Now test for whether pseudo-wetbulb is above or below/equal to zero
     # to establish whether to calculate e with respect to ice or water
     # recalc if ice
     # Find instances of ice
-    icy = np.where(w <= 0.0)
+    icy = np.where(w <= 0.0)[0]
     if (len(icy) > 0):    
     
         # Need to check whether pressure is an array or scalar
@@ -331,7 +399,9 @@ def rh(td,t,p,roundit=True):
         else: # p is an array
             f = 1 + (3.*(10**(-4.))) + ((4.18*(10**(-6.)))*p[icy])
 	
-        e[icy] = 6.1115*f*np.exp(((23.036 - (td[icy]/333.7))*td[icy]) / (279.82+td[icy]))
+        es[icy] = 6.1115*f*np.exp(((23.036 - (t[icy]/333.7))*t[icy]) / (279.82+t[icy]))
+	
+	#print('ICY ',es)
 
 #    if (w <= 0.0):
 #        f = 1 + (3.*(10**(-4.))) + ((4.18*(10**(-6.)))*p)
@@ -341,6 +411,13 @@ def rh(td,t,p,roundit=True):
 
     if (roundit == True):
         r = np.round(r*10.)/10.
+
+
+    # At the end return a scalar if it was a scalar to start with
+    if (ScalarFlag == 1):
+    
+        # We need a scalars to return
+	r = r[0]
 	
     return r 
 
@@ -378,6 +455,20 @@ def wb(td,t,p,roundit=True):
 
     '''
     
+    # Make sure everything is a numpy array
+    # At the end return a scalar if it was a scalar to start with
+    # T and Td have to be identical length so if one is a scalar then the other should be
+    ScalarFlag = 0
+    if (np.isscalar(td) == True):
+    
+        # We have scalars so temporarily make them numpy arrays
+	td = np.array([td])
+        t = np.array([t])
+	p = np.array([p])
+	
+	# Set the scalar flag to 1 so that we convert back again
+	ScalarFlag = 1    
+
     w = None
 
     e = None
@@ -394,7 +485,7 @@ def wb(td,t,p,roundit=True):
     # to establish whether to calculate e with respect to ice or water
     # recalc if ice
     # Find instances of ice
-    icy = np.where(w <= 0.0)
+    icy = np.where(w <= 0.0)[0]
     if (len(icy) > 0):    
     
         # Need to check whether pressure is an array or scalar
@@ -417,6 +508,11 @@ def wb(td,t,p,roundit=True):
 
     if (roundit == True):
         w = np.round(w*10.)/10.
+	
+    if (ScalarFlag == 1):
+    
+        # Return a scalar
+	w = w[0]
 	
     return w 
 
@@ -479,6 +575,21 @@ def td_from_vap(e,p,t,roundit=True):
     td = 10.0
 
     '''
+
+    # Make sure everything is a numpy array
+    # At the end return a scalar if it was a scalar to start with
+    # T and Td have to be identical length so if one is a scalar then the other should be
+    ScalarFlag = 0
+    if (np.isscalar(e) == True):
+    
+        # We have scalars so temporarily make them numpy arrays
+	e = np.array([e])
+        t = np.array([t])
+	p = np.array([p])
+	
+	# Set the scalar flag to 1 so that we convert back again
+	ScalarFlag = 1    
+
     td = None
     
     # First calculate an estimated dew point T
@@ -501,7 +612,7 @@ def td_from_vap(e,p,t,roundit=True):
     # to establish whether to calculate td with respect to ice or water
     # recalc if ice
     # Find instances of ice
-    icy = np.where(w <= 0.0)
+    icy = np.where(w <= 0.0)[0]
 #    pdb.set_trace()
     if (len(icy) > 0):    
 
@@ -537,5 +648,10 @@ def td_from_vap(e,p,t,roundit=True):
 
     if (roundit == True):
         td = np.round(td*10.)/10.
+	
+    if (ScalarFlag == 1):
+    
+        # Return a scalar
+	td = td[0]
 	    
     return td